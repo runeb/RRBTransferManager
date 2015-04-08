f5dbb977e33179bfedfcd154456a4666f24981a1
Rune Botten <rbotten@gmail.com>
2015-04-08 21:25:00 +0200

    Initial Commit

---
+++ RRBTransferManager/RRBTransferManager.h
@@ -0,0 +1,19 @@
+//
+//  RRBTransferManager.h
+//  RRBTransferManager
+//
+//  Created by Rune Botten on 08.04.15.
+//  Copyright (c) 2015 Rune Botten. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RRBTransferManager.
+FOUNDATION_EXPORT double RRBTransferManagerVersionNumber;
+
+//! Project version string for RRBTransferManager.
+FOUNDATION_EXPORT const unsigned char RRBTransferManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RRBTransferManager/PublicHeader.h>
+
+
